e036542c9c9fccce75f3000b7031cba9ddafc92e
Ali Othman <alioth840@gmail.com>
2024-07-05 17:31:14 +0200

    Finalizing the project

--- my-book/src/components/Header/Header.jsx
+++ my-book/src/components/Header/Header.jsx
@@ -1,27 +1,21 @@
-import React from "react";
+import React from 'react';
 import Navbar from "../Navbar/Navbar";
 import SearchForm from "../SearchForm/SearchForm";
-import "./Header.css"
+import "./Header.css";
 
-const Header = () =>{
-    return(
-        <div className="holder">
-            <header className="header">
-                <Navbar></Navbar>
-                <div className="header-content flex flex-c text-center text-white">
-                    <h2 className="header-title text-capitalize">
-                        find your book of choice
-                    </h2>
-                    <br></br>
-                    <p className="header-text fs-18">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae excepturi consequuntur non illo natus quam, incidunt sunt earum voluptate voluptatibus aut nemo perferendis rerum adipisci dicta voluptas blanditiis soluta odio!
-                    </p>
-                    <SearchForm></SearchForm>
+const Header = () => {
+    return (
+        <div className='holder'>
+            <header className='header'>
+                <Navbar />
+                <div className='header-content flex flex-c text-center text-white'>
+                    <h2 className='header-title text-capitalize'>find your book of choice.</h2><br />
+                    <p className='header-text fs-18 fw-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam beatae sapiente quibusdam consequatur perspiciatis facere laboriosam non nesciunt at id repudiandae, modi iste? Eligendi, rerum!</p>
+                    <SearchForm />
                 </div>
             </header>
-            
         </div>
     )
-};
+}
 
-export default Header;
+export default Header
--- my-book/src/index.js
+++ my-book/src/index.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import {BrowserRouter,Routes,Route} from "react-router-dom";
+import {
+  BrowserRouter, Routes, Route
+} from 'react-router-dom';
 import { AppProvider } from './Context';
+import './index.css';
 import Home from './pages/Home/Home';
-import About from './pages/About/About';
-import BookList from './components/BookListe/BookList';
-import BookDetails from './components/BookDetails/BookDetails';
-
+import About from "./pages/About/About";
+import BookList from "./components/BookListe/BookList";
+import BookDetails from "./components/BookDetails/BookDetails";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <AppProvider>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element ={<Home></Home>}></Route>
-        <Route path='about' element ={<About></About>}></Route>
-        <Route path='book' element={<BookList></BookList>}></Route>
-        <Route path='/book/:id' element={<BookDetails></BookDetails>}></Route>
+        <Route path="/" element={<Home />}>
+          <Route path="about" element={<About />} />
+          <Route path="book" element={<BookList />} />
+          <Route path="/book/:id" element={<BookDetails />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </AppProvider>
 );
-
-
